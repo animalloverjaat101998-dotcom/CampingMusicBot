e7c4fef740edb2ffbd1176574c97a4e6ba8d8e7b
animalloverjaat101998-dotcom <animalloverjaat101998@gmail.com>
2026-06-04 20:00:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+STRING_SESSION = os.getenv("STRING_SESSION")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
